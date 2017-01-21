64d0b2c7eb08437b5ae238e3c6d6fa900852d236
turboc1208 <chip.cox@coxdogs.com>
2017-01-20 20:25:47 -0600

    First Release

---
+++ weatherAlert.py
@@ -0,0 +1,137 @@
+###################################################################
+# Weather Alert - Poll for alerts from weatherunderground and alert to HA
+# Written By : Chip Cox  20JAN2017
+#
+###################################################################
+#
+# Use:
+#   Add the following lines to your appDaemon config file
+#     [weatherAlert]
+#     module=weatherAlert
+#     class=weatheralert
+#     alerts = {"HEA","TOR"}
+#
+#   Possible values for alerts are : type	Translated
+#                                    HUR	Hurricane Local Statement
+#                                    TOR	Tornado Warning
+#                                    TOW	Tornado Watch
+#                                    WRN	Severe Thunderstorm Warning
+#                                    SEW	Severe Thunderstorm Watch
+#                                    WIN	Winter Weather Advisory
+#                                    FLO	Flood Warning
+#                                    WAT	Flood Watch / Statement
+#                                    WND	High Wind Advisory
+#                                    SVR	Severe Weather Statement
+#                                    HEA	Heat Advisory
+#                                    FOG	Dense Fog Advisory
+#                                    SPE	Special Weather Statement
+#                                    FIR	Fire Weather Advisory
+#                                    VOL	Volcanic Activity Statement
+#                                    HWW	Hurricane Wind Warning
+#                                    REC	Record Set
+#                                    REP	Public Reports
+#                                    PUB	Public Information Statement
+##################################################################################
+#
+# Visit the weatherunderground at the following link for more information about this API
+# https://www.wunderground.com/weather/api/d/docs?d=data/alerts
+##################################################################################
+import appdaemon.appapi as appapi
+import requests
+from requests.auth import HTTPDigestAuth
+import json
+import time
+import datetime
+             
+class weatheralert(appapi.AppDaemon):
+
+  def initialize(self):
+    self.LOGLEVEL="INFO"
+    self.alertlog={}
+    self.log("Weather Alert App")
+    self.key="c54290c5f59273ed"
+    self.state="TN"
+    self.city="Cordova"
+    self.run_minutely(self.getAlerts,start=None)
+
+  # overrides appdaemon log file to handle application specific log files
+  # to use this you must set self.LOGLEVEL="DEBUG" or whatever in the initialize function
+  # although technically you could probably set it anywhere in the app if you wanted to
+  # just debug a function, although you probably want to set it back when you get done
+  # in the function or the rest of the program will start spewing messages
+  def log(self,message,level="INFO"):
+    levels = {                                          # these levels were taken from AppDaemon's files which were taken from python's log handler
+              "CRITICAL": 50,
+              "ERROR": 40,
+              "WARNING": 30,
+              "INFO": 20,
+              "DEBUG": 10,
+              "NOTSET": 0
+            }
+
+    if hasattr(self, "LOGLEVEL"):                        # if the LOGLEVEL attribute has been set then deal with whether to print or not.
+      if levels[level]>=levels[self.LOGLEVEL]:           # if the passed in level is >= to the desired LOGLevel the print it.
+        super().log("{} - {}".format(level,message))
+    else:                                                # the LOGLEVEL attribute was not set so just do the log file normally
+      super().log("{}".format(message),level)
+
+  ###########################
+  def getAlerts(self,kwargs):
+    # Get Alert Data
+    url = "http://api.wunderground.com/api/{}/alerts/q/{}/{}.json".format(self.key,self.state,self.city)
+
+    myResponse = requests.get(url)
+    self.log("myResponse.status_code={}".format(myResponse.status_code),"DEBUG")
+
+    # For successful API call, response code will be 200 (OK)
+    if( not myResponse.ok):
+       myResponse.raise_for_status()
+    else:
+      # Loading the response data into a dict variable
+      # json.loads takes in only binary or string variables so using content to fetch binary content
+      # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
+      jData = json.loads(myResponse.content.decode('utf-8'))
+      if self.LOGLEVEL=="DEBUG":
+        filename=self.config["AppDaemon"]["app_dir"] + "/" + "samplealert.json"
+        with open(filename) as json_data:
+          jData=json.load(json_data)         
+
+      # load list of desired alerts from appdaemon config file under heading for this app
+      self.desired_alerts=self.args["alerts"]
+      self.log("The response contains {0} properties".format(len(jData)),"DEBUG")
+      if len(jData)==0:                                                              # if there aren't any alerts clean out the alertlog and skip the rest.
+        self.alertlog={}
+        self.log("No Alerts at this time","INFO")
+      else:
+        for alert in jData["alerts"]:                                                  # Loop through all the alerts
+          alert["key"]=alert["type"]+alert["expires"]                                  # setup a unique reproducable key for each alert 
+          self.log("alert[type]={}".format(alert["type"]),"DEBUG")
+          if alert["type"] in self.desired_alerts:                                     # is this an alert type we are interested in
+            if not alert["key"] in self.alertlog:                                      # if the key is in the alertlog we have already alerted on it
+              if self.timefromstring(alert["expires"])<datetime.datetime.now():        # has this alert expired?
+                self.log("Alert has expired {}".format(alert),"INFO")
+              else:                                                                    # nope it has not expired
+                self.alertlog[alert["key"]]=alert["expires"]                           # put the key and expire date into the alertlog so we don't show it again
+                self.log("this is an alert we are interested in")
+                #self.log("Alert {} issued {} expires {} {}".format(alert["description"],alert["date"],alert["expires"],alert["message"]))
+                                                                                       # Alert using a persistent notification ( you could add other methods of alerting here too)
+                self.call_service("persistent_notification/create",title="Weather Alert",message=alert["message"])
+            else:                                                                      # we have already notified on this so don't do it again
+              self.log("Alert already in list","DEBUG")
+          else:                                                                        # there is an alert but we aren't interested in this type
+            self.log("we are not interested in alert type {}".format(alert["type"]),"INFO")
+
+
+  #######################
+  # Had some problems getting weather undergrounds date format to behave so I had to parse it out myself
+  #######################
+  def timefromstring(self,instr):
+    # This is weather undergrounds timestamp format.  Notice the "on" in the middle.  This caused a problem.
+    # h:mm AM TZZ on month dd, yyyy
+
+    strtime=instr[0:instr.find("on")-1]
+    strdate=instr[instr.find("on")+3:]
+    self.log("strtime {} strdate {}".format(strtime,strdate),"DEBUG")
+    tdate=datetime.datetime.strptime(strdate + " " + strtime,"%B %d, %Y %I:%M %p %Z")
+    self.log("tDate {}".format(tdate),"DEBUG")
+    return tdate
